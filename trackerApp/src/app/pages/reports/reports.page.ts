d162bdcabb5e622ba92fd09168e101ada3e71d3b
spookypendo <usmchome@hotmail.com>
2019-07-23 15:21:59 +0300

    Add reports function

--- trackerApp/src/app/pages/reports/reports.page.ts
+++ trackerApp/src/app/pages/reports/reports.page.ts
@@ -13,5 +13,73 @@ export class ReportsPage implements OnInit {
 
   ngOnInit() {
   }
-
+  reportsBar = {
+    title : {
+      text: 'Weekly summary',
+      subtext: '22 - 28 July 2019'
+    },
+    tooltip : {
+        trigger: 'axis',
+        axisPointer : {
+            type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+        }
+    },
+    legend: {
+        data: [ 'Missed', 'Tracked', 'Appointment'],
+        x:'right',
+        orient:'vertical'
+    },
+    grid: {
+        left: '3%',
+        right: '4%',
+        bottom: '3%',
+        containLabel: true
+    },
+    xAxis:  {
+        type: 'value'
+    },
+    yAxis: {
+        type: 'category',
+        data: [ 'Mon', 'Tues', 'Wedn', 'Thur', 'Fri', 'Sat', 'Sun']
+    },
+    series: [
+        {
+            name: 'Missed',
+            type: 'bar',
+            stack: '总量',
+            label: {
+                normal: {
+                    show: true,
+                    position: 'insideRight'
+                }
+            },
+            data: [320, 302, 301, 334, 390, 330, 320]
+        },
+        {
+            name: 'Tracked',
+            type: 'bar',
+            stack: '总量',
+            label: {
+                normal: {
+                    show: true,
+                    position: 'insideRight'
+                }
+            },
+            data: [120, 132, 101, 134, 90, 230, 210]
+        },
+        {
+            name: 'Appointment',
+            type: 'bar',
+            stack: '总量',
+            label: {
+                normal: {
+                    show: true,
+                    position: 'insideRight'
+                }
+            },
+            data: [220, 182, 191, 234, 290, 330, 310]
+        }
+    ]
+};
 }
+
